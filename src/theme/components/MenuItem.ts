e7393c2fdaa539e9aa70166a68ad784bc5cf06ba
Aleksei Sorokin <aso@anadeainc.com>
2022-07-12 19:28:03 +0400

    menu item

---
+++ src/theme/components/MenuItem.ts
@@ -0,0 +1,10 @@
+import { colors } from '../colorsLight';
+export const MuiMenuItem = {
+	styleOverrides: {
+		root: {
+			'&.Mui-selected': {
+				backgroundColor: colors.action.hover,
+			},
+		},
+	},
+};
