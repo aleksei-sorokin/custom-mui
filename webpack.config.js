7d48f98f86da192007343b3013dc7b546c00057b
Aleksei Sorokin <aso@anadeainc.com>
2022-07-12 13:17:57 +0400

    file hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const path = require('path');
+const buildVersion = process.env.VERSION;
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: "umd",
-        clean: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    externals: {
-        react: 'react'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)?$/,
-                use: ['ts-loader'],
-                exclude: /node_modules/
-            }
-        ],
-    }
-}
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  externals: {
+    react: 'react',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
